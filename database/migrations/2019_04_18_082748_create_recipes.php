fbdf37adf28076e5795ada79822e25c190f09091
DevilRep <flaringrep@gmail.com>
2019-04-18 15:23:38 +0300

    B1: Added migrations

---
+++ database/migrations/2019_04_18_082748_create_recipes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('element_id');
+            $table->unsignedInteger('component_id');
+            $table->timestamps();
+
+            $table->foreign('element_id')->references('id')->on('elements')->onDelete('cascade');
+            $table->foreign('component_id')->references('id')->on('elements')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
