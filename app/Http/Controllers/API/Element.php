5edf280ca1729cadfa6079831c89f586f9cc52aa
DevilRep <flaringrep@gmail.com>
2019-04-23 19:29:10 +0300

    G1: Added API part

---
+++ app/Http/Controllers/API/Element.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use App\Models\AvailableElement;
+
+class Element extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'items' => AvailableElement::with('element')->get()
+        ]);
+    }
+}
