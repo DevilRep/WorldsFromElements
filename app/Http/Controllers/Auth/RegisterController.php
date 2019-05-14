d2dd927e11622610261f89f273b3e8b5e7efb7b5
DevilRep <flaringrep@gmail.com>
2019-05-14 17:13:54 +0300

    B5: moved User model, added vue-router

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -10,17 +10,6 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
     /**
@@ -59,7 +48,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return User
      */
     protected function create(array $data)
     {
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 Route::namespace('API')->group(function () {
     Route::resource('elements', 'AvailableElements')->only(['index', 'store']);
     Route::resource('game', 'Game')->only(['index', 'store']);
