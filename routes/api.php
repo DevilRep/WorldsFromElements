167a81ceaf0691c60bf172dc49853d05aa85ecc9
DevilRep <flaringrep@gmail.com>
2019-05-15 12:36:34 +0300

    G6: added backend

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::namespace('API')->group(function () {
+    Route::post('/login', 'Auth@login');
     Route::resource('elements', 'AvailableElements')->only(['index', 'store']);
     Route::resource('game', 'Game')->only(['index', 'store']);
 });
